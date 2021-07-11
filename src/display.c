9c08a782166e4c12f2ee59061f82322e45d1b895
damian28102000 <damian28102000@gmail.com>
2021-07-11 09:31:38 +0200

    Ortographic projection.

--- src/display.c
+++ src/display.c
@@ -9,7 +9,8 @@ int window_height = 600;
 
 bool initialize_window(void) 
 {
-    if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0) 
+    {
         fprintf(stderr, "Error initializing SDL.\n");
         return false;
     }
@@ -57,6 +58,14 @@ void draw_grid(void)
     }
 }
 
+void draw_pixel(int x, int y, uint32_t color)
+{
+    if(x >= 0 && x < window_width && y >= 0 && y < window_height)
+    {
+        color_buffer[(window_width * y) + x] = color;
+    }
+}
+
 void draw_rect(int x, int y, int width, int height, uint32_t color) 
 {
     for (int i = 0; i < width; i++) 
@@ -65,7 +74,7 @@ void draw_rect(int x, int y, int width, int height, uint32_t color)
         {
             int current_x = x + i;
             int current_y = y + j;
-            color_buffer[(window_width * current_y) + current_x] = color;
+            draw_pixel(current_x, current_y, color);
         }
     }
 }
