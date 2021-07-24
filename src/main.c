fb8022c852fa2698f6b57e971d75a5a349c55eed
damian28102000 <damian28102000@gmail.com>
2021-07-24 20:54:58 +0200

    Implementation of a function drawing a filled triangle.

--- src/main.c
+++ src/main.c
@@ -189,20 +189,20 @@ void render(void)
     {
         triangle_t triangle = triangles_to_render[i];
 
-        // Draw vertex points
-        draw_rect(triangle.points[0].x, triangle.points[0].y, 3, 3, 0xFFFFFF00);
-        draw_rect(triangle.points[1].x, triangle.points[1].y, 3, 3, 0xFFFFFF00);
-        draw_rect(triangle.points[2].x, triangle.points[2].y, 3, 3, 0xFFFFFF00);
+        // Draw filled triangle
+        draw_filled_triangle(
+            triangle.points[0].x, triangle.points[0].y, // vertex A
+            triangle.points[1].x, triangle.points[1].y, // vertex B
+            triangle.points[2].x, triangle.points[2].y, // vertex C
+            0xFFFFFFFF
+        );
 
         // Draw unfilled triangle
         draw_triangle(
-            triangle.points[0].x,
-            triangle.points[0].y,
-            triangle.points[1].x,
-            triangle.points[1].y,
-            triangle.points[2].x,
-            triangle.points[2].y,
-            0xFF00FF00
+            triangle.points[0].x, triangle.points[0].y, // vertex A
+            triangle.points[1].x, triangle.points[1].y, // vertex B
+            triangle.points[2].x, triangle.points[2].y, // vertex C
+            0xFF000000
         );
     }
 
